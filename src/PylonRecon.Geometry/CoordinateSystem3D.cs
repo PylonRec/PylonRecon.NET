732390ac25cd967a265f502f42230fdc473f983f
Brandon Wong <29965003+brandonw3612@users.noreply.github.com>
2022-10-05 22:56:40 +0800

    set CoordinateSystem3D vector as normalized by default

--- src/PylonRecon.Geometry/CoordinateSystem3D.cs
+++ src/PylonRecon.Geometry/CoordinateSystem3D.cs
@@ -3,16 +3,22 @@ namespace PylonRecon.Geometry;
 public class CoordinateSystem3D
 {
     private static CoordinateSystem3D? _standard;
-    public static CoordinateSystem3D Standard => _standard ??= new()
+
+    public static CoordinateSystem3D Standard =>
+        _standard ??= new(Point3D.Origin, (1d, 0d, 0d), (0d, 1d, 0d), (0d, 0d, 1d));
+
+    public CoordinateSystem3D(Point3D systemOrigin, Vector3D vectorOx, Vector3D vectorOy, Vector3D vectorOz)
     {
-        SystemOrigin = Point3D.Origin,
-        VectorOX = (1, 0, 0),
-        VectorOY = (0, 1, 0),
-        VectorOZ = (0, 0, 1)
-    };
+        SystemOrigin = systemOrigin;
+        VectorOx = vectorOx.Normalize();
+        VectorOy = vectorOy.Normalize();
+        VectorOz = vectorOz.Normalize();
+        if (VectorOz != (VectorOx ^ VectorOy))
+            throw new ArithmeticException("Specified vectors cannot make a coordinate system.");
+    }
     
-    public Point3D SystemOrigin { get; set; }
-    public Vector3D VectorOX { get; set; }
-    public Vector3D VectorOY { get; set; }
-    public Vector3D VectorOZ { get; set; }
+    public Point3D SystemOrigin { get; }
+    public Vector3D VectorOx { get; }
+    public Vector3D VectorOy { get; }
+    public Vector3D VectorOz { get; }
 }
