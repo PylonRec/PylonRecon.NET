b665b6e89cec34dfdc8f873f26c8be16b7c4f870
Brandon Wong <29965003+brandonw3612@users.noreply.github.com>
2022-10-05 22:56:51 +0800

    implement CoordinateSystemConverter3D

--- src/PylonRecon.Geometry/CoordinateSystemConverter3D.cs
+++ src/PylonRecon.Geometry/CoordinateSystemConverter3D.cs
@@ -2,16 +2,40 @@ namespace PylonRecon.Geometry;
 
 public class CoordinateSystemConverter3D
 {
-    public CoordinateSystem3D From { get; set; }
-    public CoordinateSystem3D To { get; set; }
+    private readonly CoordinateSystem3D _from;
+    private readonly CoordinateSystem3D _to;
+
+    public CoordinateSystemConverter3D(CoordinateSystem3D from, CoordinateSystem3D to)
+    {
+        _from = from;
+        _to = to;
+    }
 
     public IEnumerable<Point3D> Convert(IEnumerable<Point3D> points)
     {
-        yield return null;
+        foreach (var point in points)
+        {
+            var absolute =
+                _from.SystemOrigin
+                    .MoveBy(point.X * _from.VectorOx)
+                    .MoveBy(point.Y * _from.VectorOy)
+                    .MoveBy(point.Z * _from.VectorOz);
+            var relative = _to.SystemOrigin.VectorTo(absolute);
+            yield return (relative * _to.VectorOx, relative * _to.VectorOy, relative * _to.VectorOz);
+        }
     }
     
     public IEnumerable<Point3D> ConvertBack(IEnumerable<Point3D> points)
     {
-        yield return null;
+        foreach (var point in points)
+        {
+            var absolute =
+                _to.SystemOrigin
+                    .MoveBy(point.X * _to.VectorOx)
+                    .MoveBy(point.Y * _to.VectorOy)
+                    .MoveBy(point.Z * _to.VectorOz);
+            var relative = _from.SystemOrigin.VectorTo(absolute);
+            yield return (relative * _from.VectorOx, relative * _from.VectorOy, relative * _from.VectorOz);
+        }
     }
 }
