ea62f77311339f2315cfad7940134a9de14bbad2
Brandon Wong <29965003+brandonw3612@users.noreply.github.com>
2022-10-13 20:32:04 +0800

    extend PointCloud to make it work more like a list of CloudPoint's

--- src/PylonRecon/PointCloud.cs
+++ src/PylonRecon/PointCloud.cs
@@ -4,8 +4,13 @@ using PylonRecon.Geometry;
 
 namespace PylonRecon;
 
-public sealed class PointCloud : IEnumerable<CloudPoint>
+public sealed class PointCloud : IList<CloudPoint>
 {
+    public PointCloud()
+    {
+        _points = new();
+    }
+    
     public PointCloud(IEnumerable<Point3D> locations)
     {
         _points = locations.Select(p => new CloudPoint()
@@ -37,8 +42,36 @@ public sealed class PointCloud : IEnumerable<CloudPoint>
     public (double, double) YLimits => (Locations.Min(static p => p.Y), Locations.Max(static p => p.Y));
     
     public (double, double) ZLimits => (Locations.Min(static p => p.Z), Locations.Max(static p => p.Z));
+
+    public void Add(CloudPoint item) => _points.Add(item);
+
+    public void AddRange(IEnumerable<CloudPoint> items) => _points.AddRange(items);
+
+    public void Clear() => _points.Clear();
+
+    public bool Contains(CloudPoint item) => _points.Contains(item);
+
+    public void CopyTo(CloudPoint[] array, int arrayIndex) => _points.CopyTo(array, arrayIndex);
     
+    public bool Remove(CloudPoint item) => _points.Remove(item);
+
+    public int Count => _points.Count;
+    
+    public bool IsReadOnly => false;
+    
+    public int IndexOf(CloudPoint item) => _points.IndexOf(item);
+
+    public void Insert(int index, CloudPoint item) => _points.Insert(index, item);
+
+    public void RemoveAt(int index) => _points.RemoveAt(index);
+
+    public CloudPoint this[int index]
+    {
+        get => _points[index];
+        set => _points[index] = value;
+    }
+
     public IEnumerator<CloudPoint> GetEnumerator() => _points.GetEnumerator();
-    
+
     IEnumerator IEnumerable.GetEnumerator() => _points.GetEnumerator();
 }
