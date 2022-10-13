c24494726862d041700c6548a03ef623042466cd
Brandon Wong <29965003+brandonw3612@users.noreply.github.com>
2022-10-13 20:26:41 +0800

    add default values for members in CloudPoint structure

--- src/PylonRecon/CloudPoint.cs
+++ src/PylonRecon/CloudPoint.cs
@@ -6,7 +6,7 @@ namespace PylonRecon;
 public sealed class CloudPoint
 {
     public Point3D Location { get; init; } = Point3D.Origin;
-    public Color Color { get; init; }
+    public Color Color { get; init; } = Color.Black;
     public Vector3D Normal { get; init; } = Vector3D.Zero;
-    public double Intensity { get; init; }
+    public double Intensity { get; init; } = 0d;
 }
