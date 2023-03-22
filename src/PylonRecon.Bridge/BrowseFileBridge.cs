90f06a3a26d94358207c0313456c65d9d1661637
Brandon Wong <29965003+brandonw3612@users.noreply.github.com>
2023-03-22 16:31:51 +0800

    rename Shared project to Bridge

---
+++ src/PylonRecon.Bridge/BrowseFileBridge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PylonRecon.Shared;
+
+public class BrowseFileBridge
+{
+    private static BrowseFileBridge? _instance;
+    public static BrowseFileBridge Instance => _instance ??= new();
+
+    public Action<string>? FileOpenAction { private get; set; }
+    public void OpenFile(string filePath)
+    {
+        if (FileOpenAction is null) return;
+        FileOpenAction(filePath);
+    }
+
+    public Action<PointCloud>? PointCloudLoadedAction { private get; set; }
+    public void LoadPointCloud(PointCloud cloud)
+    {
+        if (PointCloudLoadedAction is null) return;
+        PointCloudLoadedAction(cloud);
+    }
+}
